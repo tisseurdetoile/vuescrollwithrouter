f1abc63756489d6474589a1599a3488f0d095afe
TisseurDeToile <webmaster@tisseurdetoile.net>
2021-04-18 17:49:02 +0200

    feat(wave-ui): correct a la carte loadind

--- src/main.js
+++ src/main.js
@@ -1,13 +1,24 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import WaveUI from 'wave-ui'
+
+// Import Wave UI from the src directory & create the Vue app.
+import WaveUI from 'wave-ui/src/wave-ui/core'
 
 const app = createApp(App)
 
+// Import only the components you need. e.g.
+import { WApp } from 'wave-ui/src/wave-ui/components'
+
 app.use(router)
 
+app.use(WaveUI, {
+  components: { WApp },
+})
+
+// Instantiate the WaveUI class and register it in the Vue instance.
 new WaveUI(app, {
   // Some Wave UI options.
 })
+
 app.mount('#app')
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
 module.exports = {
+  transpileDependencies: ['wave-ui'], // ! \\
+  // You don't need this part if you haven't done step 2.
+  // ----------------------------------------------------
+  css: {
+    loaderOptions: {
+      // `additionalData` was called `prependData` prior sass-loader 9.
+      sass: { additionalData: '@import "@/scss/_variables.scss";' },
+    },
+  },
+  // ----------------------------------------------------
   publicPath: '/vuescrollwithrouter/',
   productionSourceMap: true,
 }
